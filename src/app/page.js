782cb010cb18fb12245fc3c90d3c60ed654fa8bd
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2024-03-01 20:09:54 +0500

    homework

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import "./globals.css"
+
+export default function Home() {
+  return (
+    <>
+      <div className="header">
+        <div className="container">
+          <nav>
+            <p>lorem1000</p>
+          </nav>
+        </div>
+      </div>
+
+      <div className="footer">
+
+      </div>
+    </>
+  );
+}
